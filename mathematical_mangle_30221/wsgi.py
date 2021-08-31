63cc46506810b238d4d26a6232d05a557473927d
Crowdbotics <team@crowdbotics.com>
2021-08-31 14:54:02 +0000

    Update models

--- mathematical_mangle_30221/wsgi.py
+++ mathematical_mangle_30221/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mathematical_mangle_30221.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mathematical_mangle_30221.settings")
 
 application = get_wsgi_application()
